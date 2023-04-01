3ec9d36d01f8f2c3246d4516b33682af065b8e33
Andrey Emelianov <621077@mail.ru>
2023-04-01 17:18:53 +0300

    сделан компонент хедер, добавлены стили

--- src/components/TheHeader/TheHeader.tsx
+++ src/components/TheHeader/TheHeader.tsx
@@ -1,9 +1,9 @@
+import { ThemeSwitcher } from 'components/ThemeSwitcher';
 import styles from './TheHeader.module.scss';
 
-interface TheHeaderProps { }
-
-export const TheHeader = ({ }: TheHeaderProps) => (
-  <div className={styles.TheHeader} data-testid="TheHeader">
-    TheHeader Component
-  </div>
+export const TheHeader = () => (
+	<div className={styles.header}>
+		<div className={styles.logo}>devfinder</div>
+		<ThemeSwitcher />
+	</div>
 );
