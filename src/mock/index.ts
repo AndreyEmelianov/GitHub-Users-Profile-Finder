0d6031ae0b2b0460be5d8b615de740ac1c11eab8
Andrey Emelianov <621077@mail.ru>
2023-04-01 18:20:05 +0300

    добавил компонент отражающий статистику юзера

---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { GithubUser } from 'types';
+
+export const defaultUser: GithubUser = {
+	login: 'octocat',
+	id: 1,
+	avatar_url: 'https://github.com/images/error/octocat_happy.gif',
+	name: 'monalisa octocat',
+	company: 'GitHub',
+	blog: 'https://github.com/blog',
+	location: 'San Francisco',
+	bio: 'There once was...',
+	twitter_username: 'monatheoctocat',
+	public_repos: 2,
+	followers: 20,
+	following: 0,
+	created_at: '2008-01-14T04:33:35Z',
+};
