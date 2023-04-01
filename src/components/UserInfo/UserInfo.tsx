34c2652ea191461c62c35dd1da7e3999ae802110
Andrey Emelianov <621077@mail.ru>
2023-04-01 19:38:51 +0300

    заметил ошибку в импорте иконок и исправил

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,7 +1,7 @@
-import { ReactComponent as CompanyIcon } from 'assets/icom-company.svg';
-import { ReactComponent as LocationIcon } from 'assets/icom-location.svg';
-import { ReactComponent as TwitterIcon } from 'assets/icom-twitter.svg';
-import { ReactComponent as BlogIcon } from 'assets/icom-website.svg';
+import { ReactComponent as CompanyIcon } from 'assets/icon-company.svg';
+import { ReactComponent as LocationIcon } from 'assets/icon-location.svg';
+import { ReactComponent as TwitterIcon } from 'assets/icon-twitter.svg';
+import { ReactComponent as BlogIcon } from 'assets/icon-website.svg';
 
 import { LocalGithubUser } from 'types';
 import { InfoItem, InfoItemProps } from 'components/InfoItem';
